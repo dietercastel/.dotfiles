c602ea7adfd0c7f5d26e9866e69ab7d93efdf4f2
Dieter Castel <dietercastel@gmail.com>
2019-10-03 09:59:26 +0200

    Bugfix in parsing term param.

--- utils/classlang.py
+++ utils/classlang.py
@@ -55,6 +55,6 @@ def classifyLanguage(filename, maxlines):
 if __name__ == '__main__':
     #Figure out why it still gets executed on import.
     print('Executing');
-    if len(sys.argv > 1):
+    if len(sys.argv) > 1:
         filename =  sys.argv[1]
         print(classifyLanguage(filename,maxlines))
